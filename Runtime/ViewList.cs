db3e80c9eda93d5cbb17a0592e1c33643a33cb64
mnicolas94 <miguelcok27@gmail.com>
2022-11-22 14:40:41 -0500

    FEAT: add some useful methods to ViewList class

--- Runtime/ViewList.cs
+++ Runtime/ViewList.cs
@@ -1,8 +1,11 @@
-﻿﻿using System.Collections.Generic;
+﻿﻿using System;
+ using System.Collections.Generic;
+ using System.Linq;
  using TNRD;
  using UnityEngine;
+ using Object = UnityEngine.Object;
 
-namespace ModelView
+ namespace ModelView
 {
     public class ViewList : MonoBehaviour
     {
@@ -10,18 +13,75 @@ namespace ModelView
 
         [SerializeField] private Transform _viewsContainer;
 
-        public void PopulateModels(IList<object> models)
+        private Dictionary<object, IView> _modelViewDictionary = new Dictionary<object, IView>();
+
+        public bool GetViewFromModel<T>(object model, out T view)
         {
+            if (_modelViewDictionary.TryGetValue(model, out var iview))
+            {
+                if (iview is T casted)
+                {
+                    view = casted;
+                    return true;
+                }
+            }
+
+            view = default;
+            return false;
+        }
+
+        public List<T> GetViews<T>()
+        {
+            return _modelViewDictionary.Values.Cast<T>().ToList();
+        }
+        
+        public IView Add(object model)
+        {
+            var existsView = _viewProvider.Value.TryGetViewForModel(model, out var view);
+            if (existsView)
+            {
+                _modelViewDictionary[model] = view;
+                if (view is Component componentView)
+                {
+                    componentView.transform.SetParent(_viewsContainer, worldPositionStays: false);
+                }
+
+                return view;
+            }
+
+            return null;
+        }
+
+        public void Remove(object model)
+        {
+            if (_modelViewDictionary.TryGetValue(model, out var view))
+            {
+                if (view is Object viewObject)
+                {
+                    Destroy(viewObject);
+                }
+
+                _modelViewDictionary.Remove(model);
+            }
+        }
+
+        public void Clear()
+        {
+            var models = _modelViewDictionary.Keys.ToString();
             foreach (var model in models)
             {
-                var exists = _viewProvider.Value.TryGetViewForModel(model, out var view);
-                if (exists)
-                {
-                    if (view is Component componentView)
-                    {
-                        componentView.transform.SetParent(_viewsContainer, worldPositionStays: false);
-                    }
-                }
+                Remove(model);
+            }
+            
+            _modelViewDictionary.Clear();
+        }
+        
+        public void PopulateModels(IList<object> models)
+        {
+            Clear();
+            foreach (var model in models)
+            {
+                Add(model);
             }
         }
     }
